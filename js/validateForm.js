d7115b3c64340c3f5fb0ea487502f9e65040a057
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2021-09-24 14:58:46 +0200

    post contact form

--- js/validateForm.js
+++ js/validateForm.js
@@ -80,3 +80,31 @@ setInterval(function () {
     subject.style.border = "1px solid var(--grey)";
     message.style.border = "1px solid var(--grey)";
 }, 6000)
+
+const formSubmissionHandler = (event) => {
+    event.preventDefault();
+  
+    const formElement = event.target,
+      { action, method } = formElement,
+      body = new FormData(formElement);
+  
+    fetch(action, {
+      method,
+      body
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // Determine if the submission is not valid
+        if (isFormSubmissionError(response)) {
+          // Handle the case when there are validation errors
+        }
+        // Handle the happy path
+      })
+      .catch((error) => {
+        // Handle the case when there's a problem with the request
+      });
+  };
+  
+  const formElement = document.querySelector("form");
+  
+  formElement.addEventListener("submit", formSubmissionHandler);
