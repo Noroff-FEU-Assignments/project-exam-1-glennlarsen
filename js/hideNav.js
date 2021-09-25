7d4a96d7ebdcd1d8660b12774f945f69af8853e0
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2021-09-25 18:20:37 +0200

    Back to top button + new fonts

--- js/hideNav.js
+++ js/hideNav.js
@@ -1,5 +1,6 @@
 let lastScrollTop;
 const navigation = document.querySelector("#navbar");
+const toTop = document.querySelector("#toTop")
 
 window.addEventListener("scroll", function(){
 
@@ -7,9 +8,27 @@ window.addEventListener("scroll", function(){
 
     if(scrollTop > lastScrollTop) {
         navigation.style.top = "-486px";
+        toTop.style.display = "block";
+        toTop.style.bottom = "-100px"
     } else {
         navigation.style.top = "0";
+        toTop.style.bottom = "0"
     }
+
 
     lastScrollTop = scrollTop;
 });
+
+toTop.onclick = function() {
+    toTop.style.display = "none";
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
